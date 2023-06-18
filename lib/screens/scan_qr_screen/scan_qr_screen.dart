52e7bf710ee45c1bdf74d83da5052c61a3d12af7
fz3hra <mujorefaatimah@gmail.com>
2023-06-18 09:59:54 +0400

    :bug: fix bug on encrypt

--- lib/screens/scan_qr_screen/scan_qr_screen.dart
+++ lib/screens/scan_qr_screen/scan_qr_screen.dart
@@ -50,12 +50,13 @@ class _ScanQrScreenState extends State<ScanQrScreen> {
     final iv = encrypt.IV.fromUtf8(
       '5183666c72eec9e4',
     );
-    final encryptedText =
-        'Main9lQcM/imQuyPF5cqCV7tvJp02BBW1xX1ECJxHLGhDnyqrYKOL84+I3FxOBAD';
+    String? encryptedText = "encryped";
     if (result != null) {
       print("result ${result!.code}");
+      encryptedText = result!.code;
       // Main9lQcM/imQuyPF5cqCV7tvJp02BBW1xX1ECJxHLGhDnyqrYKOL84+I3FxOBAD
     }
+    print("encryptedText $encryptedText");
     return Scaffold(
       body: Stack(
         children: [
@@ -85,7 +86,7 @@ class _ScanQrScreenState extends State<ScanQrScreen> {
                     encrypt.AES(key, mode: encrypt.AESMode.cbc),
                   );
 
-                  final decrypted = encrypter.decrypt64(encryptedText, iv: iv);
+                  final decrypted = encrypter.decrypt64(encryptedText!, iv: iv);
 
                   print("decrypted $decrypted");
 
@@ -107,23 +108,7 @@ class _ScanQrScreenState extends State<ScanQrScreen> {
                   );
                 },
                 // () {
-                //   // ! do this after click on popup button to proceed
-                // final key =
-                //     encrypt.Key.fromUtf8('bf3c199c2470cb477d907b1e0917c17b');
-                // final iv = encrypt.IV.fromUtf8(
-                //   '5183666c72eec9e4',
-                // );
-                // final encryptedText =
-                //     'Main9lQcM/imQuyPF5cqCV7tvJp02BBW1xX1ECJxHLGhDnyqrYKOL84+I3FxOBAD';
 
-                // final encrypter = encrypt.Encrypter(
-                //   encrypt.AES(key, mode: encrypt.AESMode.cbc),
-                // );
-
-                // final decrypted = encrypter.decrypt64(encryptedText, iv: iv);
-
-                // print("decrypted $decrypted");
-                // },
                 style: ElevatedButton.styleFrom(
                   padding: const EdgeInsets.all(12),
                   shape: RoundedRectangleBorder(
